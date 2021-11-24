279bde45b3f1cea00de8f798cc69781a30f888b9
juain <chiclyel@gmail.com>
2021-11-24 22:09:31 +0900

    analyze 파일에 광고 배너 추가

--- lib/screens/analyze_katalk_choice.dart
+++ lib/screens/analyze_katalk_choice.dart
@@ -8,6 +8,20 @@ import 'package:file_picker/file_picker.dart';
 import 'package:katalkhae/widget/date_choice_dialog.dart';
 import 'dart:async';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+const Map<String, String> UNIT_ID = kReleaseMode
+    ? {
+  'ios': '[YOUR iOS AD UNIT ID]',
+  'android': 'ca-app-pub-3016785609393816~4692289058',
+}
+    : {
+  'ios': '',
+  'android': 'ca-app-pub-3940256099942544/6300978111'
+  //광고 샘플 https://developers.google.com/admob/android/test-ads?hl=ko
+  //실제 'ca-app-pub-3016785609393816/8056818992',
+};
 
 class KatalkChoice extends StatelessWidget {
   const KatalkChoice({Key? key}) : super(key: key);
@@ -22,6 +36,20 @@ class KatalkChoice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //광고
+    TargetPlatform os = Theme.of(context).platform;
+
+    BannerAd banner = BannerAd(
+      listener: AdListener(
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {},
+        onAdLoaded: (_) {},
+      ),
+      size: AdSize.banner,
+      adUnitId: UNIT_ID[os == TargetPlatform.iOS ? 'ios' : 'android']!,
+      request: AdRequest(),
+    )..load();
+
     return Scaffold(
         appBar: AppBar(
           title: const Text(
@@ -34,34 +62,61 @@ class KatalkChoice extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.only(top: 0, left: 20, right: 20),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const SizedBox(
-                height: 10.0,
-              ),
-              const Text('해석할 카카오톡 대화를 선택해주세요.',
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  )),
-              const SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      KatalkListButton(),
-                      SizedBox(
-                        height: 15.0,
+            children: [
+              Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    const Text('해석할 카카오톡 대화를 선택해주세요.',
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            KatalkListButton(),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+              //광
+              Container(
+                height: 50,
+                child: banner == null
+                    ?  Container(
+                  child:const Text(
+                    'bad',
+                    style: TextStyle(
+                      letterSpacing: 1.0,
+                      fontSize: 26,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                )
+                    : AdWidget(
+                  ad: banner,
                 ),
               )
-            ],
+        ]
           ),
         ));
   }
